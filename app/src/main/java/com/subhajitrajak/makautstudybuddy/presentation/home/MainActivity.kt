5bc8fb20a080b6fe48a60ee95fddc1d6b907fbcc
Subhajit Rajak <subhajitrajak2001@gmail.com>
2025-05-14 18:11:41 +0530

    Refactor: Updated details activity layout and added a toast for books
    - Changed TextView IDs in `activity_details.xml` from generic names (`textView7`, `subjectName`) to more descriptive names (`bookTypeTitle`, `bookDescription`).
    - Added a placeholder image for "books_sample".
    - Added a placeholder image for "notes_sample".
    - Added a Toast message in `MainActivity` that displays "Don't worry, it's coming soon..." when the "Books" section is clicked.

--- app/src/main/java/com/subhajitrajak/makautstudybuddy/presentation/home/MainActivity.kt
+++ app/src/main/java/com/subhajitrajak/makautstudybuddy/presentation/home/MainActivity.kt
@@ -86,6 +86,10 @@ class MainActivity : AppCompatActivity() {
                 startActivity(Intent(this@MainActivity, DownloadedFilesActivity::class.java))
             }
 
+            books.setOnClickListener {
+                showToast(this@MainActivity, "Don't worry, it's coming soon...")
+            }
+
             settings.setOnClickListener {
                 startActivity(Intent(this@MainActivity, SettingsActivity::class.java))
             }
